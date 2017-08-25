af12b36b2e18953cc22faa27f96f92b3e1b6c499
Carl Castello <carlcastello@yahoo.com>
2017-08-25 17:13:30 -0600

    overlay on gallery

--- SantosHouse/HouseForSale/houseforsale/static/js/gallery.js
+++ SantosHouse/HouseForSale/houseforsale/static/js/gallery.js
@@ -1,4 +1,17 @@
 $(function() {
     var boo = $('.Gallery .gallery img').attr('src');
-    $('.Gallery .gallery img').attr('href',boo);
+    $('.Gallery .gallery a').attr('href',boo);
+
+    $('.Gallery .gallery').each(function () {
+        $(this).magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true
+            },
+            fixedContentPos: false
+        });
+    });
 });
+
